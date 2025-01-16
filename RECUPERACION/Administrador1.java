dbf8ef3c350233dda753de4a09ac800350f4126f
JhonnFer <jhoncasq23@gmail.com>
2025-01-16 08:56:25 -0500

    Prototipo1

--- RECUPERACION/Administrador1.java
+++ RECUPERACION/Administrador1.java
@@ -29,6 +29,7 @@ public class Administrador1 {
     private JButton listarButton; // Botón para listar productos
     private JButton listarButton1; // Botón para listar usuarios
     private JTable table1;
+    private JTable table2;
 
     // Listas para almacenar productos y usuarios
     private ArrayList<String> productos = new ArrayList<>();
